1b2ee6ae3a0cf0d044ea471f4aaec3b534c7c7cb
Sodablueblue <sodablueblue@gmail.com>
2016-02-08 11:42:53 +0800

    Re-construction

--- app/wechat/controller.js
+++ app/wechat/controller.js
@@ -1,7 +1,5 @@
 var crypto = require('crypto');
 var xml2js = require('xml2js');
-var templates = require('./templates');
-var q = require('q');
 
 exports.authen = function(req, res){
 	var signature = req.query.signature;
@@ -22,38 +20,38 @@ exports.authen = function(req, res){
 	}
 };
 
-exports.subscribe = function(req, res, next){
+exports.parseXml = function(req, res, next){
 	req.xml = '';
 	req.setEncoding('utf8');
 	req.on('data', function(chunck){
 		req.xml += chunck;
 	});
 	req.on('end', function(){
-		next();
+		var parser = new xml2js.Parser();
+		parser.parseString(req.xml, function(err, result){
+			if(err){
+				console.log(err);
+				process.exit(1);
+			}
+			req.xml = result.xml;
+			next();
+		});
 	});
-	
 };
 
-exports.parseXml = function(req, res, next){
-	var parser = new xml2js.Parser();
-	parser.parseString(req.xml, function(err, result){
-		if(err){
-			console.log(err);
-			process.exit(1);
-		}
-		req = result;
-		var type = req.xml.MsgType;
-		var toUser = req.xml.FromUserName;
-		var fromUser = req.xml.ToUserName;
-		var date = new Date();
-		var time = date.getTime();
-		if(type == 'event'){
-			var content = '欢迎关注Badmango!';
+exports.dealer = function(req, res){
+	if(req.xml.MsgType == 'event'){
+		if(req.xml.Event == 'subscribe'){
+			res.end(handlers.subscribe(req.xml));
+		}else if(req.xml.Event == 'unsubscribe'){
+			res.end(handlers.unsubscribe(req.xml));
 		}else{
-			var content = '你想干嘛。。。';
+			res.end(handlers.error(req.xml, 'Unsupported Event'));
 		}
-		var reply = templates.text(toUser, fromUser, time, content);	
-			
-		res.end(reply);
-	});
-};	
+	}else if(req.xml.MsgType == 'text'){
+		res.end(handlers.text(req.xml));
+	}else{
+		res.end(handlers.error(req.xml, 'Unsupported message type'));
+	}
+};
+
---
+++ app/wechat/handlers.js
@@ -0,0 +1,17 @@
+var templates = require('./templates.js');
+
+exports.text = function(xml){
+
+};
+
+exports.subscribe = function(xml){
+
+};
+
+exports.unsubscribe = function(xml){
+
+};
+
+exports.error = function(xml, message){
+
+};
